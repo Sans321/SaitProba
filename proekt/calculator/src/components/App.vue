f02a1196e53d27a7b99b39b9fa0a01edea6afd51
Svita Artem <hacker2019af@bk.ru>
2021-05-13 18:54:31 +0500

    proekt

---
+++ proekt/calculator/src/components/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <Page class="app">
+        <ActionBar class="zagalovok" title="Регистрация"/>
+        <ActionBar class="podzaga" title="Имя пользователя*"/>
+        <StackLayout>
+            <TextField class="input" v-model="newText" hint="ФИО"/>
+        </StackLayout>
+
+
+    </Page>
+</template>
+
+<script >
+   export default  {
+    data() {
+        return {
+            result: '',
+      }
+    },
+    methods:{
+            input: function(char){
+                this.result = this.result.toString();
+                this.result += char;
+            },
+            calc: function(){
+                try{
+                    this.result = eval(this.result);
+                    if (isNaN(this.result)) {
+                        throw new Error();
+                  }
+                }
+                catch{
+                    this.result = 'Ошибка';
+                }
+            },
+            del: function(){
+                this.result = '';
+            }
+    }
+  }
+</script>
+
+<style scoped>
+  
+</style>
